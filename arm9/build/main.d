37be5c10d9f13fc6875a6fc9445f0bff24cd34e1
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-12-27 19:17:36 +0900

    Delete arm9/build directory

--- arm9/build/main.d
+++
@@ -1,7 +0,0 @@
-main.o: C:/Users/S/Documents/github/lcd_lamp/arm9/source/main.c \
- C:/Users/S/Documents/github/lcd_lamp/arm9/source/i2c.h \
- C:/Users/S/Documents/github/lcd_lamp/arm9/source/types.h \
- C:/Users/S/Documents/github/lcd_lamp/arm9/source/buttons.h
-C:/Users/S/Documents/github/lcd_lamp/arm9/source/i2c.h:
-C:/Users/S/Documents/github/lcd_lamp/arm9/source/types.h:
-C:/Users/S/Documents/github/lcd_lamp/arm9/source/buttons.h:
--- arm9/build/start.d
+++
@@ -1,1 +0,0 @@
-start.o: C:/Users/S/Documents/github/lcd_lamp/arm9/source/start.s
